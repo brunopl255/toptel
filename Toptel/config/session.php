65bbb12873e30d4eb56a400719b49bd2a4f8ab8c
Brunoomsantos <32276267+Brunoomsantos@users.noreply.github.com>
2017-09-26 15:07:39 -0300

    Update 26/09 Bruno Olinto

--- Toptel/config/session.php
+++ Toptel/config/session.php
@@ -3,10 +3,10 @@
 if (!isset($_SESSION)) {
     session_start();
 }
-if ((!isset($_SESSION['login']) == true) and ( !isset($_SESSION['senha']) == true)) {
-    unset($_SESSION['login']);
-    unset($_SESSION['senha']);
+if ((!isset($_SESSION['cpf']) == true) and ( !isset($_SESSION['senhacryp']) == true)) {
+    unset($_SESSION['cpf']);
+    unset($_SESSION['senhacryp']);
     header('location:../index.php');
 }
-$logado = $_SESSION['login'];
-//$senha = $_SESSION['senha'];
+$logado = $_SESSION['cpf'];
+$senha = $_SESSION['senha'];
